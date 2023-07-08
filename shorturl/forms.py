6c99626a60023f7a8b39336a6f5a5e5b8ef56540
AlSaimun <abdullahalsaimun.2002@gmail.com>
2023-07-08 23:58:29 +0600

    urlshortener

---
+++ shorturl/forms.py
@@ -0,0 +1,10 @@
+from .models import Shortener
+from django import forms
+
+
+"""Form"""
+class ShortenerForm(forms.ModelForm):
+    long_url = forms.URLField(widget= forms.URLInput(attrs={"class": "form-control form-control-lg", "placeholder": "Your URL to shorten"}))
+    class Meta:
+        model = Shortener
+        fields = ('long_url',)
